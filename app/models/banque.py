811a705dc5d084705f7f8175ba9157724b19e2ac
Barry Ali <pavelnemesis288@gmail.com>
2025-09-11 20:53:21 +0000

    config de model et schema pour table banque

---
+++ app/models/banque.py
@@ -0,0 +1,15 @@
+## banque.py pour gerer le model sqlalchemy de la table banques
+from sqlalchemy import String, Column, Integer
+from sqlalchemy.orm import relationship
+from app.core.database import Base
+
+
+class Banque(Base):
+  
+  __tablename__ = "banques"
+  
+  id_banque: int = Column(Integer, primary_key=True, index=True, nullable=False)
+  nom_banque: str = Column(String(50), index=True, nullable=False)
+  adresse: str = Column(String(50), index=True, nullable=False)
+
+  users = relationship("User", back_populates="banque")
