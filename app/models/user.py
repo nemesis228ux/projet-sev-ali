1a8830e6eba1fdcafc2c94af25fb0b5d423a9433
Barry Ali <pavelnemesis288@gmail.com>
2025-09-11 12:44:32 +0000

    Creation model et schema user

--- app/models/user.py
+++ app/models/user.py
@@ -1,1 +1,28 @@
 # contient le model sqlalchemy de la table user
+from app.core.database import Base
+from sqlalchemy import Column, Integer, String, Enum as SqlEnum, Date, ForeignKey
+from enum import Enum as UserEnum
+from sqlalchemy.orm import relationship
+
+
+class UserRole(UserEnum):
+  
+  client = "client"
+  admin = "admin"
+
+
+class User(Base):
+  
+  __tablename__ = "users"
+
+  id_user: int = Column(Integer, primary_key=True, index=True, nullable=False)
+  nom_user: str = Column(String(50), index=True, nullable=False)
+  email: str = Column(String(50), index=True, nullable=False)
+  hashed_password: str = Column(String(180), index=True, nullable=False)
+  addresse: str = Column(String(50), index=True, nullable=False)
+  date_naissance: str = Column(Date, index=True, nullable=False)
+  role: str = Column(SqlEnum(UserRole), default=UserRole.client, index=True, nullable=False)
+
+  id_banque: int = Column(Integer, ForeignKey("banques.id_banque"), index=True, nullable=False)
+
+  banque = relationship("Banque", back_populates="users")
