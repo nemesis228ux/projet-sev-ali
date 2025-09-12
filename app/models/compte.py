ad56815cbebe5c794ac86c89746d8361eaf46bc1
Sevtify44 <gametech228@gmail.com>
2025-09-12 10:16:00 +0000

    Ajout du modèle SQLAlchemy pour la table `comptes` et mise à jour de la relation avec les utilisateurs

---
+++ app/models/compte.py
@@ -0,0 +1,70 @@
+# Contient le model sqlalchemy de la table compte
+
+from app.core.database import Base
+from sqlalchemy import Column, Integer, String, Enum as SqlEnum, DateTime, ForeignKey, Double
+from sqlalchemy.sql import func
+from enum import Enum
+from .user import User
+from sqlalchemy.orm import relationship
+class AccountTypes(str, Enum):
+    """Enumartions des types de compte possibles"""
+
+    COURANT = "courant"
+
+    EPARGNE = "epargne"
+
+
+class Compte(Base):
+    """Modèles SQLAlchemy de la table `comptes`"""
+
+    __tablename__ = "comptes"
+
+    id_compte : int = Column(
+        Integer,
+        primary_key = True,
+        autoincrement = True,
+        index = True,
+    )
+
+    numero_compte : str = Column(       # Finalement en str pour plus de simplicité
+        String,
+        unique = True,
+        nullable = False,
+        index = True
+    )
+
+    type_compte : str = Column(
+        SqlEnum(AccountTypes),
+        default = AccountTypes.COURANT,
+        nullable = False
+    )
+
+    solde : float = Column(
+        Double,
+        default = 0.0,
+        index = True
+    )
+
+    date_ouverture : str = Column(
+        DateTime,
+        default = func.now()
+    )
+
+    account_owner_id : int = Column(
+        Integer,
+        ForeignKey("users.id_user"),
+        index = True
+    )
+
+
+    base_user : User = relationship(
+        "User",
+        back_populates = "comptes",
+        uselist = False
+    )
+
+    #TODO : Ajouter une relationship pour les transactions
+    #TODO : Ajouter une relationship pour les cb
+
+
+
