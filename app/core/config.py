43dce6370851aa01a1299e0742264c741342b212
Sev[404] <71104775+Sev228@users.noreply.github.com>
2025-09-13 10:38:19 +0000

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 
 load_dotenv() ## permet de charger les config depuis le fichier .env
 
-SECRET_KEY = os.getenv("SECRET_KEY", "super-cle-secret")
-ALGORITHM = os.getenv("ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRES_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRES_MINUTES", 30))
+SECRET_KEY : str = os.getenv("SECRET_KEY", "super-cle-secret")
+ALGORITHM : str = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRES_MINUTES : int = int(os.getenv("ACCESS_TOKEN_EXPIRES_MINUTES", 30))
