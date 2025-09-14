d6c0eaa0d4bf53718df65a74eb47e5455db25e00
Sevtify44 <gametech228@gmail.com>
2025-09-14 21:18:33 +0000

    Renommage des paramètres de session de base de données pour une meilleure clarté : remplacement de `bd` par `bd_session` dans les fonctions de gestion des comptes.

--- app/crud/compteCrud.py
+++ app/crud/compteCrud.py
@@ -8,12 +8,12 @@ from uuid import uuid4
 from app.models import add_all_tables
 add_all_tables()
 
-def create_new_account(bd : Session, new_account_base : AccountCreate) -> Optional[Compte]:
+def create_new_account(bd_session : Session, new_account_base : AccountCreate) -> Optional[Compte]:
     """
     Fonctionn pour ajjouter un utilisateur à la base de données
 
     Args:
-        bd: Instance de la bd
+        bd_session: Instance de la bd
         new_account_base: Informations de bases sur le nouveau compte à créer
 
     Returns:
@@ -30,13 +30,13 @@ def create_new_account(bd : Session, new_account_base : AccountCreate) -> Option
         try:
             random_num_compte = uuid4().int.__str__()[:16]      # Pour générer un numéro de compte aléatoire
             new_account.numero_compte = random_num_compte
-            bd.add(new_account)
-            bd.commit()
-            bd.refresh(new_account)
+            bd_session.add(new_account)
+            bd_session.commit()
+            bd_session.refresh(new_account)
             return new_account
 
         except IntegrityError:      # Cas ou le numéro de compte est déja présent
-            bd.rollback()
+            bd_session.rollback()
             new_account.numero_compte = uuid4().int.__str__()[:16]
             continue
 
@@ -44,12 +44,12 @@ def create_new_account(bd : Session, new_account_base : AccountCreate) -> Option
             print(f'Exception {e.__class__.__name__} : {e}')
             break
 
-def delete_user_account(bd : Session, user_id : int, account_id : int) -> bool:
+def delete_user_account(bd_session : Session, user_id : int, account_id : int) -> bool:
     """
     Fonction pour supprimer un compte de la base de donnée
 
     Args:
-        bd: Instance de la bd
+        bd_session: Instance de la bd
         user_id: Id de l'utilisateur recheché
         account_id: Id du compte recherché
 
@@ -57,23 +57,23 @@ def delete_user_account(bd : Session, user_id : int, account_id : int) -> bool:
         type: `True` si l'opération a réussi, `False` sinon
     """
     try:
-        account = get_account_by_id(bd, user_id, account_id)    # On recupère le compte à supprimer
+        account = get_account_by_id(bd_session, user_id, account_id)  # On recupère le compte à supprimer
         if not account:
             return False
 
-        bd.delete(account)
-        bd.commit()
+        bd_session.delete(account)
+        bd_session.commit()
         return True
     except Exception as e:
         print(f'Exception {e.__class__.__name__} : {e}')
         return False
 
-def get_all_accounts(bd: Session, account_type : Optional[AccountTypes] = None) -> Sequence[Compte]:
+def get_all_accounts(bd_session: Session, account_type : Optional[AccountTypes] = None) -> Sequence[Compte]:
     """
     Fonction pour recuperer tous les comptes de la bd, avec possibilité de filtrage par type de compte
 
     Args:
-        bd: Instance de la bd
+        bd_session: Instance de la bd
         account_type: Parametre optionnel pour filtrer les compte par rapport au type
 
     Returns:
@@ -87,10 +87,10 @@ def get_all_accounts(bd: Session, account_type : Optional[AccountTypes] = None) 
             Compte.type_compte == account_type
         )
 
-    return bd.scalars(query).all()
+    return bd_session.scalars(query).all()
 
 def get_user_accounts(
-        bd : Session,
+        bd_session : Session,
         user_id : int,
         account_type : Optional[AccountTypes] = None
 ) -> Optional[Sequence[Compte]]:
@@ -98,7 +98,7 @@ def get_user_accounts(
     Fonction pour recuperer tous les comptes d'un utilisateur spécifique, avec possibilité de filtrage par type de compte
 
     Args:
-        bd: Instance de la bd
+        bd_session: Instance de la bd
         user_id: Id de l'utilisateur recherché
         account_type: Parametre optionnel pour filtrer les compte par rapport au type
 
@@ -116,14 +116,14 @@ def get_user_accounts(
             Compte.type_compte == account_type
         )
 
-    return bd.scalars(query).all()
+    return bd_session.scalars(query).all()
 
-def get_account_by_id(bd : Session, user_id : int, account_id : int) -> Optional[Compte]:
+def get_account_by_id(bd_session : Session, user_id : int, account_id : int) -> Optional[Compte]:
     """
     Fonction pour recuperer un compte spécique d'un utilisateur par son user_id
 
     Args:
-        bd: Instance de la bd
+        bd_session: Instance de la bd
         user_id: Id de l'utilisateur recheché
         account_id: Id du compte recherché
 
@@ -136,14 +136,14 @@ def get_account_by_id(bd : Session, user_id : int, account_id : int) -> Optional
         Compte.account_owner_id == user_id,
         Compte.id_compte == account_id
     )
-    return bd.scalars(query).first()
+    return bd_session.scalars(query).first()
 
-def get_account_by_numero_compte(bd : Session, num_compte : str) -> Optional[Compte]:
+def get_account_by_numero_compte(bd_session : Session, num_compte : str) -> Optional[Compte]:
     """
     Fonction pour recuperer un compte spécique d'un utilisateur par son numero de compte
 
     Args:
-        bd (Session): Instance de la bd
+        bd_session (Session): Instance de la bd
         num_compte (str) : Numero de compte de l'utilisateur recheché
 
     Returns:
@@ -154,4 +154,4 @@ def get_account_by_numero_compte(bd : Session, num_compte : str) -> Optional[Com
     query = select(Compte).where(
         Compte.numero_compte == num_compte,
     )
-    return bd.scalars(query).first()
+    return bd_session.scalars(query).first()
