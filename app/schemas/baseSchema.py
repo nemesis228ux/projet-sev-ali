97bd9ab41bddb9d6670035c511bc16e4abf59a3c
Sevtify44 <gametech228@gmail.com>
2025-09-18 21:05:35 +0000

    Ajout de quelques commentaires notamment au niveau des schemas

--- app/schemas/baseSchema.py
+++ app/schemas/baseSchema.py
@@ -3,13 +3,23 @@ from pydantic import BaseModel, Field
 
 T = TypeVar("T")
 
+# Modele de base pour toutes les reponses de l'API
+#TODO: Faire des recherches et voir si çà ne serait pas mieux de mettre en classe abstraite
 class ApiBaseResponse(BaseModel, Generic[T]):
     """Scema de base pour uniformiser les réponses de l'API"""
-    ok : bool = Field(..., title="Champ pour véreifier si la reequete à réussi")
-    result : Optional[T] = Field(default=None, title='Champ de résultat', description= 'Présent seulement si la requet à réussi')
-    error: Optional[str] = None
+
+    ok : bool = Field(..., title="Champ pour véreifier si la reequete à réussi, True si la requete a reussi, False sinon")
+
+    result : Optional[T] = Field(
+        default=None, title='Champ de résultat',
+        description= 'Présent seulement si la requet à réussi'
+    )
+    error: Optional[str] = Field(
+        default=None, title='Champ des erreurs',
+        description= "Présent seulement si la requet à échouée ou si quelque chose s'est mal passé durant le traitement"
+    )
     
-    
+    # Cette methode permettra de renvoyer les reponse de succèss directement depuis les classes filles
     @classmethod
     def success_response(cls, data : Any):
         """
@@ -21,6 +31,7 @@ class ApiBaseResponse(BaseModel, Generic[T]):
         """
         return cls(ok=True, result=data, error=None)
 
+    # Cette methode permettra de renvoyer les reponse d'erreur directement depuis les classes filles
     @classmethod
     def error_response(cls, error_message : str):
         """
